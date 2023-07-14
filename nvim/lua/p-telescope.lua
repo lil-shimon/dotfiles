688d0b3fcde6383a6aebc0176613cc495b636cdf
Kenta Shimosawa <shimon101018@gmail.com>
2023-07-15 02:06:58 +0900

    feat: 🎸 add telescope file browser

--- nvim/lua/p-telescope.lua
+++ nvim/lua/p-telescope.lua
@@ -2,12 +2,19 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 
+function telescope_buffer_dir()
+  return vim.fn.expand('%:p:h')
+end
+
+local fb_actions = require 'telescope'.extensions.file_browser.actions
 
 local actions = require('telescope.actions')
+
 require('nvim-web-devicons').setup({
   override = {},
   default = true
 })
+
 require('telescope').setup {
   pickers = {
     find_files = {
@@ -29,5 +36,39 @@ require('telescope').setup {
       preview_cutoff = 100,
       preview_width = 0.6
     }
+  },
+  extensions = {
+    file_browser = {
+      theme = 'dropdown',
+      -- disables netrw add use telescope-file-browser in its place
+      hijack_netrw = true,
+      mappings = {
+        ['i'] = {
+          ['<C-w>'] = function() vim.cmd('normal vbd') end,
+        },
+        ['n'] = {
+          ['N'] = fb_actions.create,
+          ['h'] = fb_actions.goto_parent_dir,
+          ['/'] = function()
+            vim.cmd('startinsert')
+          end
+        }
+      },
+    }
   }
 }
+
+
+require('telescope').load_extension('file_browser')
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set('n', ' f', '<cmd>lua require("telescope.builtin").find_files({ no_ignore = false, hidden = true })<cr>',
+  opts)
+vim.keymap.set('n', ' r', '<cmd>lua require("telescope.builtin").live_grep()<cr>',
+  opts)
+vim.keymap.set('n', ' d', '<cmd>lua require("telescope.builtin").diagnostics()<cr>',
+  opts)
+vim.keymap.set('n', '  ',
+  '<cmd>lua require("telescope").extensions.file_browser.file_browser({ path = "%:p:h", cmd = telescope_buffer_dir(), respect_git_ignore = true, hidden = true, grouped = true, previewer = false, initial_mode = "normal", layout_config = { height = 40 } } )<cr>'
+  ,
+  opts)

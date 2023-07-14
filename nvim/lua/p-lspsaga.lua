74291b5de94edd71d727b942180debb2df322cc0
Kenta Shimosawa <shimon101018@gmail.com>
2023-07-15 01:59:28 +0900

    feat: 🎸 change saga config to my settings

--- nvim/lua/p-lspsaga.lua
+++ nvim/lua/p-lspsaga.lua
@@ -1,4 +1,5 @@
 local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 require('lspsaga').setup({
   ui = {
@@ -6,7 +7,8 @@ require('lspsaga').setup({
   }
 })
 
-keymap("n", "<leader>k", "<cmd>Lspsaga hover_doc<CR>")
+keymap("n", "K", "<cmd>Lspsaga hover_doc<CR>")
+keymap('i', '<C-k>', '<Cmd>Lspsaga signature_help<cr>', opts)
 
 keymap({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
 
@@ -15,6 +17,6 @@ keymap("n", "gd", "<cmd>Lspsaga goto_definition<CR>")
 keymap("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
 keymap("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>")
 
-keymap("n", "<leader>r", "<cmd>Lspsaga rename<CR>")
+keymap("n", "<C-r>", "<cmd>Lspsaga rename<CR>")
 
 keymap("n", "<leader>o", "<cmd>Lspsaga outline<CR>")

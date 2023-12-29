dc06a9626ce227f26e8347122643c0f3b124e677
Kenta Shimosawa <shimon101018@gmail.com>
2023-12-30 00:27:07 +0900

    feat: 🎸 ChatGPTにコメントしてもらった

--- nvim/init.lua
+++ nvim/init.lua
@@ -15,33 +15,46 @@ vim.opt.rtp:prepend(lazypath)
 
 
 require("lazy").setup({
+  -- エディタのビジュアルテーマ
   "sainnhe/everforest",
+
+  -- ステータスラインのカスタマイズ
   "nvim-lualine/lualine.nvim",
 
+  -- LSP (Language Server Protocol) と関連ツールのインストールと管理
   "williamboman/mason.nvim",
   "williamboman/mason-lspconfig.nvim",
   "neovim/nvim-lspconfig",
   "WhoIsSethDaniel/mason-tool-installer.nvim",
 
+  -- 代替ファイル切り替え
   "rmagatti/alternate-toggler",
+
+  -- オートペアリング（括弧やクォートの自動補完）
   "windwp/nvim-autopairs",
+
+  -- マルチカーソル機能
   "mg979/vim-visual-multi",
+
+  -- テキスト選択の拡張機能
   "gcmt/wildfire.vim",
+
+  -- テキスト周りの操作のカスタマイズ（サラウンド）
   {
     "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    version = "*",
     event = "VeryLazy",
     config = function()
-      require("nvim-surround").setup({
-        -- Configuration here, or leave empty to use defaults
-      })
+      require("nvim-surround").setup({})
     end
   },
 
+  -- ファイル検索や様々なリスト表示のための拡張機能
   "nvim-telescope/telescope.nvim",
   "nvim-telescope/telescope-file-browser.nvim",
   "nvim-lua/plenary.nvim",
 
+  -- オートコンプリート機能
   "hrsh7th/cmp-nvim-lsp",
   "hrsh7th/cmp-buffer",
   "hrsh7th/cmp-path",
@@ -50,16 +63,19 @@ require("lazy").setup({
   "onsails/lspkind-nvim",
   "glepnir/lspsaga.nvim",
 
+  -- スニペット機能
   "L3MON4D3/LuaSnip",
   "saadparwaiz1/cmp_luasnip",
 
+  -- TypeScriptに特化したLSP機能
   "jose-elias-alvarez/typescript.nvim",
   "jose-elias-alvarez/null-ls.nvim",
 
+  -- Treesitterベースの構文解析とハイライト
   {
-    'nvim-treesitter/nvim-treesitter',
+    "nvim-treesitter/nvim-treesitter",
     dependencies = {
-      'JoosepAlviste/nvim-ts-context-commentstring',
+      "JoosepAlviste/nvim-ts-context-commentstring",
     },
   },
   "nvim-treesitter/nvim-treesitter-context",
@@ -67,49 +83,48 @@ require("lazy").setup({
   "p00f/nvim-ts-rainbow",
   "axelvc/template-string.nvim",
 
+  -- TODOコメントのハイライトと管理
   "folke/todo-comments.nvim",
+
+  -- コメントの追加と管理
   "numToStr/Comment.nvim",
+
+  -- ターミナルのトグル
   "akinsho/toggleterm.nvim",
 
+  -- セッション管理
   "tpope/vim-obsession",
   "dhruvasagar/vim-prosession",
+
+  -- Git blame情報の表示
   "f-person/git-blame.nvim",
+
+  -- GitHub Copilotの統合
   "github/copilot.vim",
 
+  -- ファイルエクスプローラ
   "nvim-tree/nvim-tree.lua",
   "nvim-tree/nvim-web-devicons",
 
+  -- クリップボード履歴の管理
   "gbprod/yanky.nvim",
 
+  -- フラッシュ機能（検索結果のハイライト）
   {
     "folke/flash.nvim",
     event = "VeryLazy",
     opts = {},
     keys = {
-      {
-        "f",
-        mode = { "n", "x", "o" },
-        function()
-          require("flash").jump()
-        end,
-        desc = "Flash",
-      },
-      {
-        "F",
-        mode = { "n", "o", "x" },
-        function()
-          require("flash").treesitter()
-        end,
-        desc = "Flash Treesitter",
-      },
+      { "f", mode = { "n", "x", "o" }, function() require("flash").jump() end,     desc = "Flash" },
+      { "F", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     },
   },
+
+  -- ノーティフィケーションとUI改善
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {
-
-    },
+    opts = {},
     dependencies = {
       "munifTanjim/nui.nvim",
       "rcarriga/nvim-notify",

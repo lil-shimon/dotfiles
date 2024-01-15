8c753a97ab9cbd48464cc3296c21c406fb136963
Kenta Shimosawa <shimon101018@gmail.com>
2024-01-15 17:40:15 +0900

    refactor: 💡 update colorscheme of wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,7 +6,7 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'MaterialDesignColors'
 config.font_size = 16
 config.font = wezterm.font("JetBrains Mono", { weight = "Regular", stretch = "Normal", style = "Normal" })
 

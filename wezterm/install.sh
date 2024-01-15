9c88573013886ce4abe37320dbe4b973bbe14e12
Kenta Shimosawa <shimon101018@gmail.com>
2024-01-15 17:34:57 +0900

    refactor: 💡 change install wezterm shell script's dir

--- wezterm/install.sh
+++ wezterm/install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# WezTermがインストールされているかどうかをチェックします。
+if brew list wezterm &>/dev/null; then
+    echo "WezTermは既にインストールされています。"
+else
+    echo "WezTermをインストールしています..."
+    brew install --cask wezterm
+    echo "WezTermのインストールが完了しました。"
+fi
+
 # シンボリックリンクを作成する前に、既存のリンクが存在するかをチェックします。
 if [ -L ~/.wezterm.lua ]; then
     echo "既にシンボリックリンクが存在します: ~/.wezterm.lua"

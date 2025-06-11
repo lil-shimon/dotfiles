38b77db30065635bebe2e3db738179612e0a64e0
lil-shimon <shimon101018@gmail.com>
2025-06-11 15:35:39 +0900

    update install.sh to install ripgrep

--- nvim/install.sh
+++ nvim/install.sh
@@ -8,6 +8,14 @@ else
     echo "Neovim has been installed."
 fi
 
+if brew list ripgrep &>/dev/null; then
+    echo "Ripgrep is already installed."
+else
+    echo "Installing Ripgrep for faster file search..."
+    brew install ripgrep
+    echo "Ripgrep has been installed."
+fi
+
 if [ -L ~/.config/nvim ]; then
     echo "Symbolic link already exists: ~/.config/nvim"
 else
